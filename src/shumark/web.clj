691813605194bab911bd8c24e562f593bf451ff9
Si Yu <siyu798@gmail.com>
2013-01-21 01:58:10 -0500

    fixed -main

--- src/shumark/web.clj
+++ src/shumark/web.clj
@@ -2,9 +2,7 @@
   (:use [ring.adapter.jetty :only [run-jetty]])
   (:require [shumark.app :as app]))
 
-(defn -main
-  ":join? false is used to avoid blocking the repl thread."
-  [& m]
+(defn -main  [& m]
   (run-jetty #'app/app {:join? false
                         :port (Integer. (or (System/getenv "PORT") (:port m) "8080"))}))
 
